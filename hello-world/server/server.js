ba7d44d4fe4eee5d71578ca36921d86751fedc8f
Win <winsanpang@hotmail.co.uk>
2022-02-25 20:43:25 +0000

    Add folders for first two courses

---
+++ hello-world/server/server.js
@@ -0,0 +1,17 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    greeting: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    greeting: () => 'Hello GraphQL World!'
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+server.listen({ port: 9000 })
+  .then(({url}) => console.log(`Server running at ${url}`))
